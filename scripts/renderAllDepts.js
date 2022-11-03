5c4416ea83e39663b92f04fbf8af2e8393a2f239
Gabryel <gabryelmaraujo@gmail.com>
2022-11-03 10:33:40 -0300

    feat: login page & render dept

---
+++ scripts/renderAllDepts.js
@@ -0,0 +1,63 @@
+
+
+const renderAllDepts = async () => {
+
+const allDeptsUrl = "http://localhost:6278/departments"
+
+const admin = JSON.parse(localStorage.getItem("@KenzieEmpresas/LoggedUser:"))
+
+
+const renderDepts = await fetch( allDeptsUrl, {
+    method: "GET",
+    headers: {
+        "Authorization": `Bearer ${admin.token}`
+    }
+} )
+
+const depts = await renderDepts.json()
+console.log(depts)
+
+depts.forEach((e)=>{
+
+    const deptId = e.uuid
+    const deptName = e.name
+    const deptDesc = e.description
+    const companyId = e.companies.uuid
+    const companyName = e.companies.name
+
+    const deptsList = document.querySelector('.deptsUl')
+
+    deptsList.insertAdjacentHTML('afterbegin', `
+    
+        <li class="deptLi">
+            <p class="deptName">
+                ${deptName}
+            </p>
+            <p class="deptDesc">
+                ${deptDesc}
+            </p>
+            <p class="deptCompany" id="${companyId}">
+                ${companyName}
+            </p>
+            <div class="deptBttns">
+                <button id="viewDeptBttn">
+                    <img src="/images/viewbttn.svg" alt="Ver informações do departamento" class="viewDeptBttnImg">
+                </button>
+                <button id="editDeptBttn">
+                    <img src="/images/editbttn.svg" alt="Editar departamento" class="editDeptBttnImg">
+                </button>
+                <button id="delDeptBttn">
+                    <img src="/images/trashbttn.svg" alt="Deletar departamento"
+                    class="delDeptBttnImg">
+                </button>
+                
+            </div>
+        </li>
+
+    `)
+
+})
+
+}
+
+export { renderAllDepts }
