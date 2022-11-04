9bcf79e51ff2fda6d4ed383b8cf344c86e5888a7
Gabryel <gabryelmaraujo@gmail.com>
2022-11-04 19:04:06 -0300

    feat: show header function create

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -3,9 +3,10 @@
 import { renderCompanies } from "../../scripts/renderCompanies.js";
 import { homeFilter } from "../../scripts/homeFilter.js";
 import { openFilter } from "../../scripts/openFilter.js";
+import { showHeader } from "../../scripts/showHeader.js";
 
 
-
+showHeader()
 renderCompanies()
 homeFilter()
 openFilter()
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,3 +1,5 @@
 import { loginFunction } from "../../scripts/login.js";
+import { showHeader } from "../../scripts/showHeader.js";
 
 await loginFunction()
+showHeader()
